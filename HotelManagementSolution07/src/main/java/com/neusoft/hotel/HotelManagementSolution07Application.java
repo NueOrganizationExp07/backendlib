8ff8cbbd95be7c2c0015fc50364a2cdf503ed99b
ZhangSan <1733108435@qq.com>
2020-07-27 09:34:54 +0800

    项目第一次提交。

---
+++ HotelManagementSolution07/src/main/java/com/neusoft/hotel/HotelManagementSolution07Application.java
@@ -0,0 +1,13 @@
+package com.neusoft.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSolution07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSolution07Application.class, args);
+	}
+
+}
