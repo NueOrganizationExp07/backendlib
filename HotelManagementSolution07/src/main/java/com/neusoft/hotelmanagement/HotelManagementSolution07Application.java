5031e237f2876773949f66286dda2ef46e5cc8e2
ZhangSan <1733108435@qq.com>
2020-07-30 12:31:40 +0800

    王润哲 增加登录功能

--- HotelManagementSolution07/src/main/java/com/neusoft/hotelmanagement/HotelManagementSolution07Application.java
+++ HotelManagementSolution07/src/main/java/com/neusoft/hotelmanagement/HotelManagementSolution07Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages={"com.neusoft.hotelmanagement.hotel.mapper","com.neusoft.hotelmanagement.manager.mapper","com.neusoft.hotelmanagement.room.mapper","com.neusoft.hotelmanagement.customer.mapper","com.neusoft.hotelmanagement.employee.mapper"})
+@MapperScan(basePackages={"com.neusoft.hotelmanagement.hotel.mapper","com.neusoft.hotelmanagement.manager.mapper","com.neusoft.hotelmanagement.room.mapper","com.neusoft.hotelmanagement.customer.mapper","com.neusoft.hotelmanagement.employee.mapper","com.neusoft.hotelmanagement.admin.mapper"})
 public class HotelManagementSolution07Application {
 
 	public static void main(String[] args) {
---
+++ HotelManagementSolution07/src/main/java/com/neusoft/hotelmanagement/admin/model/RoleModel.java
@@ -0,0 +1,5 @@
+package com.neusoft.hotelmanagement.admin.model;
+import java.io.Serializable;
+public class RoleModel implements Serializable {
+
+}
